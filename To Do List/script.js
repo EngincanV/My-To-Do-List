dbfb9c041d0a905bdcd978b4fcca0076d7e9f996
EngincanV <43685404+EngincanV@users.noreply.github.com>
2019-02-23 13:46:04 +0300

    Add files via upload

---
+++ To Do List/script.js
@@ -0,0 +1,54 @@
+var myLi = document.getElementsByTagName("LI");
+
+//It creates a close button
+for(var i = 0; i < myLi.length ; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    //It adds span element below the li tags
+    myLi[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+for(var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = 'none';
+    }
+}
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function addActivity() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } 
+    else {
+      document.getElementById("activities").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
